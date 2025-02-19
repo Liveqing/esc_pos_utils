787616889d6441814ffdd2510c0d3fba41739c4d
Qingger <li0905203@gmail.com>
2025-02-19 19:51:20 +0800

    feat: 升级image版本;

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -145,16 +145,31 @@ class Generator {
 
     // Create a black bottom layer
     final biggerImage = copyResize(image, width: widthPx, height: heightPx);
-    fill(biggerImage, 0);
+    //fill(biggerImage, 0);
     // Insert source image into bigger one
-    drawImage(biggerImage, image, dstX: 0, dstY: 0);
+    //drawImage(biggerImage, image, dstX: 0, dstY: 0);
+
+    //fill(biggerImage, color: ColorRgb8(0, 0, 0));
+    fill(biggerImage, color: ColorRgb8(0, 0, 0));
+    // Insert source image into bigger one
+    compositeImage(biggerImage, image, dstX: 0, dstY: 0);
 
     int left = 0;
     final List<List<int>> blobs = [];
 
+    // while (left < widthPx) {
+    //   final Image slice = copyCrop(biggerImage, left, 0, lineHeight, heightPx);
+    //   final Uint8List bytes = slice.getBytes(format: Format.luminance);
+    //   blobs.add(bytes);
+    //   left += lineHeight;
+    // }
     while (left < widthPx) {
-      final Image slice = copyCrop(biggerImage, left, 0, lineHeight, heightPx);
-      final Uint8List bytes = slice.getBytes(format: Format.luminance);
+      final Image slice = copyCrop(biggerImage,
+          x: left, y: 0, width: lineHeight, height: heightPx);
+      if (slice.numChannels > 2) grayscale(slice);
+      final imgBinary =
+      (slice.numChannels > 1) ? slice.convert(numChannels: 1) : slice;
+      final bytes = imgBinary.getBytes();
       blobs.add(bytes);
       left += lineHeight;
     }
@@ -173,7 +188,8 @@ class Generator {
 
     // R/G/B channels are same -> keep only one channel
     final List<int> oneChannelBytes = [];
-    final List<int> buffer = image.getBytes(format: Format.rgba);
+    //final List<int> buffer = image.getBytes(format: Format.rgba);
+    final List<int> buffer = image.getBytes(order: ChannelOrder.rgba);
     for (int i = 0; i < buffer.length; i += 4) {
       oneChannelBytes.add(buffer[i]);
     }
@@ -577,8 +593,11 @@ class Generator {
     const bool highDensityVertical = true;
 
     invert(image);
-    flip(image, Flip.horizontal);
-    final Image imageRotated = copyRotate(image, 270);
+    //flip(image, Flip.horizontal);
+    //final Image imageRotated = copyRotate(image, 270);
+
+    flipHorizontal(image);
+    final Image imageRotated = copyRotate(image, angle: 270);
 
     const int lineHeight = highDensityVertical ? 3 : 1;
     final List<List<int>> blobs = _toColumnFormat(imageRotated, lineHeight * 8);
